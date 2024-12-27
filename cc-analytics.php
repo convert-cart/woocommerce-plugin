b60d8921276ed09cc94a7668401a222558e4978c
Udhaya Kumar <udhaya@convertcart.com>
2024-12-27 11:59:02 +0530

    Release beta version 1.3.0-beta

--- cc-analytics.php
+++ cc-analytics.php
@@ -4,9 +4,9 @@
  * Description: Official Convert Cart Analytics WordPress plugin that transforms abandoned carts into product pages, tracks user behavior, provides detailed analytics, and optimizes your store for increased conversions and revenue.
  * Author: Convert Cart
  * Author URI: https://www.convertcart.com/
- * Version: 1.2.4
+ * Version: 1.3.0-beta
  * Tested up to: 6.5.5
- * Stable Tag: 1.2.4
+ * Stable Tag: 1.3.0-beta
  * License: GPLv2 or later
  * Tags: conversion rate optimization, conversion, revenue boost
  *
@@ -26,7 +26,7 @@ defined('ABSPATH') || exit;
 function add_integration($integrations)
 {
     if (! defined('CC_PLUGIN_VERSION')) {
-        define('CC_PLUGIN_VERSION', '1.2.4');
+        define('CC_PLUGIN_VERSION', '1.3.0-beta');
     }
 
     global $woocommerce;
