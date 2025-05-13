e102385d2eebe284bed2953ae09d83c8661ddeb6
sam <ayush@convertcart.com>
2025-05-13 19:11:54 +0530

    Release beta version 1.3.4-beta

--- cc-analytics.php
+++ cc-analytics.php
@@ -4,9 +4,9 @@
  * Description: Official Convert Cart Analytics WordPress plugin that transforms abandoned carts into product pages, tracks user behavior, provides detailed analytics, and optimizes your store for increased conversions and revenue.
  * Author: Convert Cart
  * Author URI: https://www.convertcart.com/
- * Version: 1.3.4
+ * Version: 1.3.4-beta
  * Tested up to: 6.5.5
- * Stable Tag: 1.3.4
+ * Stable Tag: 1.3.4-beta
  * License: GPLv2 or later
  * Tags: conversion rate optimization, conversion, revenue boost
  * Requires at least: 5.6
